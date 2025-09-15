942cfc9ef9692c768d36a23ab5d13d7d56f5baad
misa <mischa.zeu1998@gmail.com>
2025-09-15 20:58:49 +0900

    добавил класс Cat и переписал класс App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,18 @@
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hw1 = context.getBean("helloworld", HelloWorld.class);
+        HelloWorld hw2 = context.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = context.getBean("cat", Cat.class);
+        Cat cat2 = context.getBean("cat", Cat.class);
+
+        System.out.println(hw1 == hw2);
+        System.out.println(cat1 == cat2);
+
+        context.close();
     }
 }
