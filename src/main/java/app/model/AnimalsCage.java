06344882127465244cb6fd1efe20e35f84017a7f
misa <mischa.zeu1998@gmail.com>
2025-10-10 20:44:16 +0900

    renewed the time

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -19,6 +19,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
